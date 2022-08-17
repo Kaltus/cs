31513c92511097d171a6f26c3831856bfcd5702c
chengshu <1150830511@qq.com>
2022-08-17 12:04:30 +0800

    demo01

--- TestProject/src/cn/cs/test/main.java
+++ TestProject/src/cn/cs/test/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("来啦");
+        System.out.println("hello");
     }
 }
